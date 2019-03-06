80f5e6b886386aa7b09b5ea7ab5420ca10fae83a
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2019-03-06 16:56:42 +0530

    Added classes for numberToWords and PerfectNumber.

---
+++ src/com/step/assignments/PerfectNumber.java
@@ -0,0 +1,20 @@
+package com.step.assignments;
+
+public class PerfectNumber {
+    public static void main(String[] args) {
+        System.out.println(isPerfectNumber(6));
+    }
+
+    public static boolean isPerfectNumber(int number) {
+        int sum = 0;
+        if (number < 1) {
+            return false;
+        }
+        for (int i = 1; i < number; i++) {
+            if (number % i == 0) {
+                sum += i;
+            }
+        }
+       return sum == number;
+    }
+}
