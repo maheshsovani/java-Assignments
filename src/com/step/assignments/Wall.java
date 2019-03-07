dc98f9d8345a644c2f21f598f0a401b8d20709ed
Mahesh Sovani <44019471+maheshsovani@users.noreply.github.com>
2019-03-07 09:30:42 +0530

    Added Wall and Point classes.

---
+++ src/com/step/assignments/Wall.java
@@ -0,0 +1,43 @@
+package com.step.assignments;
+
+public class Wall {
+    private double width;
+    private double height;
+
+    public Wall() {
+    }
+
+    public Wall(double width, double height) {
+        this.setHeight(height);
+        this.setWidth(width);
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if (width < 0) {
+            this.width = 0;
+            return;
+        }
+        this.width = width;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if (height < 0) {
+            this.height = 0;
+            return;
+        }
+        this.height = height;
+    }
+
+    public double getArea() {
+        return this.height * this.width;
+    }
+}
+
